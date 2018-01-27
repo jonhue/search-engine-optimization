abfb16501feab6ecf80c494c961824794a1cd86d
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-27 10:52:29 +0100

    minor bugfixes

--- lib/search_engine_optimization/railtie.rb
+++ lib/search_engine_optimization/railtie.rb
@@ -4,38 +4,42 @@ require 'rails/railtie'
 module SearchEngineOptimization
     class Railtie < Rails::Railtie
 
+        include ActionView::Helpers::AssetUrlHelper
+
         initializer 'search-engine-optimization.mozaic' do
             Mozaic.configure do |config|
 
                 config.define_component 'seo/schema' do |options|
-                    return false if options[:type].nil?
+                    false if options[:type].nil?
                 end
 
+                config.define_component 'seo/schema/aggregate_rating'
                 config.define_component 'seo/schema/article'
+                config.define_component 'seo/schema/organization'
                 config.define_component 'seo/schema/person'
 
                 config.define_component 'seo/schema/properties/articleBody' do |options|
-                    return false if options[:article_body].nil?
+                    false if options[:article_body].nil?
                 end
                 config.define_component 'seo/schema/properties/articleSection' do |options|
-                    return false if options[:article_section].nil?
+                    false if options[:article_section].nil?
                 end
                 config.define_component 'seo/schema/properties/datePublished' do |options|
-                    return false if options[:date_published].nil?
-                    options[:date_published] = options[:date_published].iso8601
+                    options[:date_published] = options[:date_published]&.iso8601
+                    false if options[:date_published].nil?
                 end
                 config.define_component 'seo/schema/properties/image' do |options|
-                    return false if options[:image].nil?
-                    options[:image] = image_url options[:image]
+                    options[:image] = image_url options[:image] unless options[:image].nil?
+                    false if options[:image].nil?
                 end
                 config.define_component 'seo/schema/properties/name' do |options|
-                    return false if options[:name].nil?
+                    false if options[:name].nil?
                 end
                 config.define_component 'seo/schema/properties/ratingValue' do |options|
-                    return false if options[:rating_value].nil?
+                    false if options[:rating_value].nil?
                 end
                 config.define_component 'seo/schema/properties/url' do |options|
-                    return false if options[:url].nil?
+                    false if options[:url].nil?
                 end
 
             end
